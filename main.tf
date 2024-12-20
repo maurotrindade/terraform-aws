2c5c9a46055d1f56e0486c1cc54fa7b58e8ab88d
Mauro Trindade <mauro.trindade@invillia.com>
2024-12-20 08:40:49 -0300

    liberando a VM para acesso e outros

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+# só vai pegar os metadados
+data "aws_secretsmanager_secret" "my_first_secret" {
+  arn = "arn:aws:secretsmanager:us-east-1:364155191442:secret:my/first/secret-In9ajk"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.my_first_secret.id
+}
+
 provider "aws" {
   region = "us-east-1"
   # vai pegar do profile local do aws cli
@@ -16,12 +25,20 @@ provider "aws" {
 
 resource "aws_vpc" "my_first_vpc" {
   cidr_block = "10.0.0.0/16" # 65.536 IPs disponíveis
+
+  tags = {
+    "Name" = "my-first-vpc"
+  }
 }
 
 resource "aws_subnet" "my_first_subnet" {
   vpc_id            = aws_vpc.my_first_vpc.id
   cidr_block        = "10.0.1.0/24" # 256 IPs disponíveis
   availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "my-first-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "my_first_gateway" {
@@ -37,9 +54,10 @@ resource "aws_eip" "my_first_eip" {
 }
 
 resource "aws_instance" "my_first_ec2" {
-  ami           = "ami-01816d07b1128cd2d"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_first_subnet.id
+  ami             = "ami-01816d07b1128cd2d"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.my_first_subnet.id
+  security_groups = [aws_security_group.my_first_sg.id]
 }
 
 # exemplo salvando o IP na SM ParameterStore
@@ -50,6 +68,43 @@ resource "aws_ssm_parameter" "my_first_paramstore" {
   value = aws_eip.my_first_eip.public_ip
 }
 
+resource "aws_route_table" "my_first_table" {
+  vpc_id = aws_vpc.my_first_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_first_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "table_line" {
+  subnet_id      = aws_subnet.my_first_subnet.id
+  route_table_id = aws_route_table.my_first_table.id
+}
+
+resource "aws_security_group" "my_first_sg" {
+  vpc_id = aws_vpc.my_first_vpc.id
+  name   = "Allow SSH"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "my_first_sg_ingress_rule" {
+  security_group_id = aws_security_group.id
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "my_first_sg_egress_rule" {
+  security_group_id = aws_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # -1 = todos os protocolos
+}
+
 output "ip" {
   value = aws_eip.my_first_eip.public_ip
 }
+
+# output "output_nada_seguro" {
+#   value = jsondecode(data.aws_secretsmanager_secret_version.current)["KEY"]
+# }
